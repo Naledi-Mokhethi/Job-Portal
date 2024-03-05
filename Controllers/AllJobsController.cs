a5889b8d6529b334d4e739c35ba5acea78a1f1ab
naledi mokhethi <naledi.mokhethi@outlook.com>
2024-03-05 21:13:52 +0200

    Initial controller intergration Work in progress

---
+++ Controllers/AllJobsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.Controllers
+{
+	public class AllJobsController : Controller
+	{
+		// GET: AllJobsController - returns all jobs
+		public ActionResult AllJobs()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/JobDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.Controllers
+{
+	public class JobDetailsController : Controller
+	{
+		public IActionResult JobDetails()
+		{
+			return View();
+		}
+	}
+}
